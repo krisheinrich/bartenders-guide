22f9adb5b72e927d843a2fb37d3ee0c8f84aa091
Kris Heinrich <krisheinrich@gmail.com>
2019-06-13 22:47:37 -0700

    Display results from CocktailDB API on search page

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,17 +1,24 @@
 import NavBar from '../components/navbar';
+import 'isomorphic-unfetch';
 
 const Search = (props) => (
   <div>
     <NavBar />
-    List for query { props.query }
+    <h2>List for query { props.query }</h2>
+    <h3>{ props.results.length } results</h3>
+    <ul>
+      { props.results.map(({ idDrink, strDrink }) => (
+        <li key={idDrink}>{ strDrink }</li>
+      )) }
+    </ul>
   </div>
 );
 
 Search.getInitialProps = async (ctx) => {
   const query = ctx.query.q;
-  // const res = await fetch('https://api.github.com/repos/zeit/next.js');
-  // const json = await res.json();
-  return { query };
+  const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`);
+  const json = await res.json();
+  return { query, results: json.drinks };
 };
 
 export default Search;

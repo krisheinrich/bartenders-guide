b9ffbee0a84ca6d9c38b4f6fdb65bdd881ce9c4f
Kris Heinrich <krisheinrich@gmail.com>
2019-06-14 20:50:56 -0700

    Add Bootstrap classes to NavBar; enable search using the Enter key; add search icon

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,4 +1,5 @@
 import Router from 'next/router';
+import SearchIcon from './icons/search';
 
 class NavBar extends React.Component {
   state = {
@@ -19,20 +20,19 @@ class NavBar extends React.Component {
   render() {
     const { searchQuery } = this.state;
     return (
-      <div>
-        <button>Hamburger</button>
-        <div className="row">
-          <input type="text" value={searchQuery} onChange={this.updateQuery}/>
-          <button onClick={this.handleSearch}>Search</button>
+      <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+        <div class="container">
+        <a class="navbar-brand" href="#">BG</a>
+          <form className="form-inline" onSubmit={(e) => { e.preventDefault(); }}>
+            <input class="form-control mr-sm-1" type="search" placeholder="Search"
+              value={searchQuery} onChange={this.updateQuery} aria-label="Search"/>
+            <button class="btn btn-outline-success my-sm-0" type="submit"
+              onClick={this.handleSearch}>
+              <SearchIcon width="16" height="16"/>
+            </button>
+          </form>
         </div>
-        <style jsx>{`
-          div {
-            display: flex;
-            flex-direction: row;
-            justify-content: space-between;
-          }
-        `}</style>
-      </div>
+      </nav>
     );
   }
 }

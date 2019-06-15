2be40bd78e31e5be29bc0c98dade8454463754d8
Kris Heinrich <krisheinrich@gmail.com>
2019-06-14 21:07:06 -0700

    Improve search bar styles

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -23,10 +23,10 @@ class NavBar extends React.Component {
       <nav class="navbar navbar-expand-md navbar-dark bg-primary">
         <div class="container">
         <a class="navbar-brand" href="#">BG</a>
-          <form className="form-inline" onSubmit={(e) => { e.preventDefault(); }}>
-            <input class="form-control mr-sm-1" type="search" placeholder="Search"
+          <form className="form-inline flex-nowrap" onSubmit={(e) => { e.preventDefault(); }}>
+            <input class="form-control" type="search" placeholder="Search"
               value={searchQuery} onChange={this.updateQuery} aria-label="Search"/>
-            <button class="btn btn-outline-success my-sm-0" type="submit"
+            <button class="btn btn-outline-success" type="submit"
               onClick={this.handleSearch}>
               <SearchIcon width="16" height="16"/>
             </button>

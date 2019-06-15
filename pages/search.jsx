6d850957997fe0a27059030302d6e372179d73c2
Kris Heinrich <krisheinrich@gmail.com>
2019-06-14 20:56:17 -0700

    Render list of search results as drink cards; use main layout on search results page; handle case where API sends null response

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,24 +1,50 @@
-import NavBar from '../components/navbar';
+import MainLayout from '../layouts/main';
+import DrinkCard from '../components/drink-card';
 import 'isomorphic-unfetch';
 
+const renderList = (results) => {
+  let cards = results.map((result) => {
+    // Prepare API data for DrinkCard props
+    const {
+      idDrink: id,
+      strDrink: name,
+      strDrinkThumb: thumbUrl,
+      strIngredient1,
+      strIngredient2
+    } = result;
+
+    let preview = strIngredient1;
+    if (strIngredient2) {
+      preview += `, ${strIngredient2}...`;
+    }
+
+    const cardProps = { id, name, thumbUrl, preview };
+
+    return (
+      <DrinkCard key={id} {...cardProps} />
+    );
+  });
+
+  return (
+    <div className="card-deck d-flex">
+      { cards }
+    </div>
+  );
+};
+
 const Search = (props) => (
-  <div>
-    <NavBar />
-    <h2>List for query { props.query }</h2>
+  <MainLayout>
+    <h2>List for query '{ props.query }'</h2>
     <h3>{ props.results.length } results</h3>
-    <ul>
-      { props.results.map(({ idDrink, strDrink }) => (
-        <li key={idDrink}>{ strDrink }</li>
-      )) }
-    </ul>
-  </div>
+    { renderList(props.results) }
+  </MainLayout>
 );
 
 Search.getInitialProps = async (ctx) => {
   const query = ctx.query.q;
   const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`);
   const json = await res.json();
-  return { query, results: json.drinks };
+  return { query, results: json.drinks || [] };
 };
 
 export default Search;

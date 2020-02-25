dd057071be7fab627a45d8bac7547d1c9dcaf968
Kris Heinrich <krisheinrich@gmail.com>
2020-02-24 18:19:24 -0800

    Add Playbill font for landing page

--- layouts/main.jsx
+++ layouts/main.jsx
@@ -7,7 +7,9 @@ export default ({ children }) => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <title>The Bartender's Guide</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
+      <link href="https://fonts.googleapis.com/css?family=Playball&display=swap" rel="stylesheet"></link>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
     </Head>
     <NavBar />
     <div className="container">

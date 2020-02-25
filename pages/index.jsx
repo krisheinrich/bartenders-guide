59e2920d724bb7fb63ed6a6b424c528debffaa3d
Kris Heinrich <krisheinrich@gmail.com>
2020-02-24 18:24:50 -0800

    Add hero image to landing page; add SearchBar instance to focus user on search first

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,36 @@
 import MainLayout from '../layouts/main';
+import SearchBar from '../components/searchbar';
+
 
 const Home = () => (
   <MainLayout>
-    <h1>The Bartender's Guide</h1>
+    <div className="hero">
+      <div className="hero-title">
+        <h1>The Bartender's Guide</h1>
+      </div>
+      <div className="container">
+        <SearchBar className="col-lg-8 offset-lg-2 mt-5" placeholder="Discover new cocktails by name or ingredient"/>
+      </div>
+    </div>
+    <style jsx>{`
+      .hero {
+        height: 100%;
+        background: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),
+                    url('/cocktail-with-mint.jpg') no-repeat center center scroll;
+        background-size: cover;
+      }
+      .hero-title {
+        margin-top: 10vh;
+        margin-bottom: 100px;
+      }
+      h1 {
+        font-family: 'Playball';
+        font-size: 92px;
+        text-align: center;
+        color: #eee;
+        text-shadow: 2px 2px 4px #000;
+      }
+    `}</style>
   </MainLayout>
 );
 

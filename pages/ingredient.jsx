f9cc1e9d1c590f24440fe1c16fd9f36e690feeaf
krisheinrich <krisheinrich@gmail.com>
2020-03-18 13:29:02 -0700

    Enhance ingredient page with image; add spacing/indentation to layout

--- pages/ingredient.jsx
+++ pages/ingredient.jsx
@@ -1,18 +1,30 @@
 import PageLayout from '../layouts/page';
 import 'isomorphic-unfetch';
 
-const Ingredient = ({ name, type, description }) => (
-  <PageLayout>
-    <div className="container">
-      <h1>{ name }</h1>
-      { type && name !== type && <h4 className="text-muted">Type of { type }</h4> }
-      <div>
-        <h4>Description</h4>
-        <p>{ description }</p>
+const Ingredient = ({ name, type, description, imgUrl }) => {
+  return (
+    <PageLayout>
+      <div className="container">
+        <h1>{ name }</h1>
+        { type && name !== type && <h4 className="text-muted">Type of { type }</h4> }
+        <div className="row mt-5">
+          <div className="col-md-4 d-flex align-items-center">
+          <img src={imgUrl} alt={name}/>
+          </div>
+          <div className="col-md-8">
+            <h4>Description</h4>
+            <p className="description mt-4">{ description }</p>
+          </div>
+        </div>
       </div>
-    </div>
-  </PageLayout>
-);
+      <style jsx>{`
+        .description {
+          text-indent: 3em;
+        }
+      `}</style>
+    </PageLayout>
+  );
+};
 
 
 Ingredient.getInitialProps = async (ctx) => {
@@ -30,7 +42,8 @@ Ingredient.getInitialProps = async (ctx) => {
   return {
     name: ingredient.strIngredient,
     description: ingredient.strDescription || 'No information available.',
-    type: ingredient.strType
+    type: ingredient.strType,
+    imgUrl: `https://www.thecocktaildb.com/images/ingredients/${ingredientName}.png`
   };
 };
 

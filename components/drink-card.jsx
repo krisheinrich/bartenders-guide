a3f9d1853b3db25ff88974b9043d0bf98d332a3d
Kris Heinrich <krisheinrich@gmail.com>
2020-05-04 23:55:58 -0700

    Remove ingredient preview from search results; center name

--- components/drink-card.jsx
+++ components/drink-card.jsx
@@ -4,8 +4,7 @@ const DrinkCard = (props) => (
   <div className="card mb-4" style={{ flex: '0 0 200px' }}>
     <img className="card-img-top" src={props.thumbUrl} alt={props.name}/>
     <div className="card-body">
-      <h5 className="card-title">{ props.name }</h5>
-      <p className="card-text">{ props.preview }</p>
+      <h5 className="card-title text-center">{ props.name }</h5>
     </div>
     <div className="card-footer d-flex justify-content-center">
       <Link href={`/drink?id=${props.id}`}>

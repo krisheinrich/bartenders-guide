6989e5206008f87d07bd1e16ce6908cb91682232
Kris Heinrich <krisheinrich@gmail.com>
2021-03-24 16:38:04 -0700

    Add logo icon to navbar; change navbar color and add shadow

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,10 +3,10 @@ import SearchBar from './searchbar';
 
 const NavBar = () => (
   <>
-  <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+  <nav className="navbar navbar-expand-md navbar-dark">
     <div className="container">
       <Link href="/">
-        <a className="navbar-brand">BG</a>
+        <img className="logo-icon" src="cocktail.png"/>
       </Link>
       <SearchBar className="form-inline"/>
     </div>
@@ -14,10 +14,13 @@ const NavBar = () => (
   <style jsx>{`
     .navbar {
       flex: 0 0 auto;
+      background: rgba(52, 58, 64, 0.3);
+      box-shadow: 0 1px 8px #444;
     }
-    .navbar-brand {
-      font-family: 'Playball', cursive;
-      font-size: 24px;
+    .logo-icon {
+      width: 44px;
+      cursor: pointer;
+      margin: 6px 0;
     }
   `}</style>
   </>
